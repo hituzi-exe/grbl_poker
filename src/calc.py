504cf80e418381416f19a204f447c22ca9657ed5
hituzi-exe <sparychory@gmail.com>
2018-12-27 15:20:07 +0900

    add convert method

--- src/calc.py
+++ src/calc.py
@@ -1,33 +1,36 @@
 import numpy as np
 
-import time
-
 
 class Calc:
+
+    # (0x2c000 == 0b1111 0000 0000 0000 0000)
+    JOKER = 0xF0000
+
     def createDeck(self):
-        # (0x2001 == 0b10 0000 0000 0001)
-        # (0x3fff == 0b11 1111 1111 1111)
-        num = (0x2001 << np.arange(13, dtype=np.int32)) & 0x3fff
-        suit = 0x0001 << (np.arange(4, dtype=np.int32) + 14)
+        # S = suit bit, N = num bit
+        # card : SSSS xxxN NNNN NNNN NNNN
+        num = (0x0001 << np.arange(13, dtype=np.int32)) & 0x1fff
+        suit = 0x0001 << (np.arange(4, dtype=np.int32) + 16)
 
         numx, suitx = np.ix_(num, suit)
         deck = suitx + numx
 
-        return deck.flatten()
+        return np.append(deck.flatten(), Calc.JOKER)
 
     def getMaxExpectation(self, hand1, hand2, hand3, hand4, hand5):
-        return 0
+        return -1
 
     def getHandRank(self, hand1, hand2, hand3, hand4, hand5):
-        pass
+        return -1
 
-#   一番右側から連続してる0の数だけ右にシフト
-#   x / (x & -x)
+    def convert(self, hand):
+        # set(hand1)
+        return -1
 
 
 def main():
     c = Calc()
-    c.createDeck()
+    print(c.createDeck())
 
 
 if __name__ == '__main__':
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from src.calc import Calc
 
 
@@ -8,17 +9,31 @@ class TescCalc(unittest.TestCase):
         c = Calc()
         self.assertEqual(c.getMaxExpectation('', '', '', '', '',), 0)
 
+    def test_createDeck(self):
+        c = Calc()
+        deck = c.createDeck()
+        self.assertTrue(
+            (deck == np.array([65537, 131073, 262145, 524289, 65538, 131074,
+                               262146, 524290, 65540, 131076, 262148, 524292,
+                               65544, 131080, 262152, 524296, 65552, 131088,
+                               262160, 524304, 65568, 131104, 262176, 524320,
+                               65600, 131136, 262208, 524352,  65664, 131200,
+                               262272, 524416, 65792, 131328, 262400, 524544,
+                               66048, 131584, 262656, 524800, 66560, 132096,
+                               263168, 525312, 67584, 133120, 264192, 526336,
+                               69632, 135168, 266240, 528384, 983040])).all())
+
     def test_convert_s1(self):
         c = Calc()
-        self.assertEqual(c.convert('s1'), 0x00010000000000000001)
+        self.assertEqual(c.convert('s1'), 0b00010000000000000001)
 
     def test_convert_dt(self):
         c = Calc()
-        self.assertEqual(c.convert('dt'), 0x00100000001000000000)
+        self.assertEqual(c.convert('dt'), 0b00100000001000000000)
 
     def test_convert_Joker(self):
         c = Calc()
-        self.assertEqual(c.convert('J0'), 0x11110000000000000000)
+        self.assertEqual(c.convert('J0'), 0b11110000000000000000)
 
     def test_JOKER(self):
         c = Calc()
