cc2dfe430a34fb624910f4da59e4d442fdf7467b
hituzi-exe <sparychory@gmail.com>
2018-12-27 13:04:44 +0900

    modify createDeck

--- src/calc.py
+++ src/calc.py
@@ -5,40 +5,28 @@ import time
 
 class Calc:
     def createDeck(self):
-        num = 1 << np.arange(13, dtype=np.int32)
-        sute = 1 << (np.arange(4, dtype=np.int32) + 13)
+        # (0x2001 == 0b10 0000 0000 0001)
+        # (0x3fff == 0b11 1111 1111 1111)
+        num = (0x2001 << np.arange(13, dtype=np.int32)) & 0x3fff
+        suit = 0x0001 << (np.arange(4, dtype=np.int32) + 14)
 
-        roopcount = 1000000
+        numx, suitx = np.ix_(num, suit)
+        deck = suitx + numx
 
-        # time1
-        start = time.time()
-        for i in range(0, roopcount):
-            deck = np.asarray([num + n for n in sute])
+        return deck.flatten()
 
-        elapsed_time = time.time() - start
-        print("time1:{0}".format(elapsed_time) + "[sec]")
+    def getMaxExpectation(self, hand1, hand2, hand3, hand4, hand5):
+        return 0
 
-        # time2
-        start = time.time()
-        for i in range(0, roopcount):
-            numx, sutex = np.ix_(num, sute)
-            deck2 = sutex + numx
+    def getHandRank(self, hand1, hand2, hand3, hand4, hand5):
+        pass
 
-        elapsed_time = time.time() - start
-        print("time2:{0}".format(elapsed_time) + "[sec]")
-
-        # result
-        # time1:10.297203063964844[sec]
-        # time2:8.98630404472351[sec]
-
-
-def getMaxExpectation(self, hand1, hand2, hand3, hand4, hand5):
-    return 0
+#   一番右側から連続してる0の数だけ右にシフト
+#   x / (x & -x)
 
 
 def main():
     c = Calc()
-
     c.createDeck()
 
 
